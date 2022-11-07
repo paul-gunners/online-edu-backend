ada6b837aae53cb689b1026b9c61c4f704c7edc5
Paul Q D Li <lqd_nchu@163.com>
2022-11-07 23:59:49 +0800

    get ali keyid and keysecret from spring container, enable MSM module client discovery in order to invoke by api-gateway

--- service/service-msm/src/main/java/com/hsbc/cmb/connect/msmservice/utils/ConstantMsmUtils.java
+++ service/service-msm/src/main/java/com/hsbc/cmb/connect/msmservice/utils/ConstantMsmUtils.java
@@ -2,7 +2,9 @@ package com.hsbc.cmb.connect.msmservice.utils;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ConstantMsmUtils implements InitializingBean {
 
     //读取配置文件内容
